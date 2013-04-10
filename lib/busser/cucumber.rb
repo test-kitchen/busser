7608eb281aee2ee65c2e40b4fd29635ce0536fb8
Fletcher Nichol <fnichol@nichol.ca>
2013-04-10 00:45:44 -0600

    Add more cucumber steps for vendor paths.

--- lib/busser/cucumber.rb
+++ lib/busser/cucumber.rb
@@ -69,6 +69,21 @@ Then(/^the suite file "(.*?)" should contain exactly:$/) do |file, content|
   check_exact_file_content(file_name, content)
 end
 
+Then(/^the vendor directory named "(.*?)" should exist$/) do |name|
+  directory = File.join(ENV['BUSSER_ROOT'], "vendor", name)
+  check_directory_presence([directory], true)
+end
+
+Then(/^the vendor directory named "(.*?)" should not exist$/) do |name|
+  directory = File.join(ENV['BUSSER_ROOT'], "vendor", name)
+  check_directory_presence([directory], false)
+end
+
+Then(/^the vendor file "(.*?)" should contain "(.*?)"$/) do |file, content|
+  file_name = File.join(ENV['BUSSER_ROOT'], "vendor", file)
+  check_file_content(file_name, content, true)
+end
+
 Then(/^a gem named "(.*?)" is installed with version "(.*?)"$/) do |name, ver|
   unbundlerize do
     run_simple(unescape("gem list #{name} --version #{ver} -i"), true, nil)
